40fd1fbf478a633444321ff8350d1f00b35623ba
ZetBrush <zetbrush@gmail.com>
2019-02-07 14:27:52 +0400

    test changes for so

--- ReactAndroid/src/main/java/com/facebook/react/bridge/ReactBridge.java
+++ ReactAndroid/src/main/java/com/facebook/react/bridge/ReactBridge.java
@@ -7,7 +7,11 @@
 
 package com.facebook.react.bridge;
 
+import android.util.Log;
+
 import com.facebook.soloader.SoLoader;
+
+import java.lang.reflect.Method;
 
 public class ReactBridge {
   private static boolean sDidInit = false;
@@ -15,6 +19,12 @@ public class ReactBridge {
     // No locking required here, worst case we'll call into SoLoader twice
     // which will do its own locking internally
     if (!sDidInit) {
+      Method[] arr = SoLoader.class.getDeclaredMethods();
+      for (int i = 0; i < arr.length; i++) {
+        if(arr[i].toString().contains("loadLibrary")) {
+          Log.d("SoLoaderCheck", arr[i].toString());
+        }
+      }
       SoLoader.loadLibrary("reactnativejni");
       sDidInit = true;
     }
